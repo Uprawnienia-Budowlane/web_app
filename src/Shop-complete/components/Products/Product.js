62978bbcd628c49ce98b9b5bb20bfec9cb5a2443
Vauxc34 <kaxowy12@gmail.com>
2021-11-17 17:49:28 +0100

    commerce js integration product

--- src/Shop-complete/components/Products/Product.js
+++ src/Shop-complete/components/Products/Product.js
@@ -9,15 +9,17 @@ const Product = ({ product }) => {
         <>
     <div className="mb-3 rounded-2xl bg-blue-50 flex flex-col w-80" style={{minWidth: '300px'}}>
     <div className="mx-4 mt-8 flex flex-row justify-between">
-        <p className="text-blue-500 opacity-60 my-auto">PAKIET {product.name}</p>
+        <p className="text-blue-500 opacity-60 my-auto">PAKIET</p>
         <div className="my-auto"><TagIcon/></div>
     </div>
-    <p className="mx-4 mt-4 text-xl text-blue-500 font-semibold"></p>
+    <p className="mx-4 mt-4 text-xl text-blue-500 font-semibold">{product.name}</p>
     <div className="mx-4 mt-2 flex flex-row">
-        <p className="text-xl text-blue-500 font-medium"></p>
+        <p className="text-xl text-blue-500 font-medium">{product.price.formatted} zł</p>
         <p className="ml-2 text-blue-500 opacity-60 my-auto font-medium">/ za sztukę</p>
     </div>
-    <p className="mx-4 mt-4 mb-2 overflow-clip text-blue-500 text-sm"></p>
+    <p className="mx-4 mt-4 mb-2 overflow-clip text-blue-500 text-sm">
+        <p dangerouslySetInnerHTML={{ __html: product.description }}/>
+    </p>
 
      
 
