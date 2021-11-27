0fa8137274285f9c851d762ed6dfb357ae30262f
Vauxc34 <kaxowy12@gmail.com>
2021-11-27 13:31:10 +0100

    admin panel update

--- src/Admin-panel-complete/AdminPanel.js
+++ src/Admin-panel-complete/AdminPanel.js
@@ -26,10 +26,26 @@ MessageIcon,
 PenIcon, 
 ShoppingCartIcon,
 SettingsIcon
-
 } from "../Icons";
 import LicenseLogo from './Images/logo_component.svg'
 import Logo from './components/Logo'
+
+/* shortcode */
+
+import NewsIcon from './Images/article-line 2.svg'
+import FileList from './Images/file-list-2-line 2.svg'
+import NotificationList from './Images/notification-2-line 2.svg'
+import LightBuble from './Images/lightbulb-line 1.svg'
+import ShieldLIst from './Images/shield-keyhole-fill 3.svg'
+
+/* */
+
+/* calendar */
+
+import PenIconC from './Images/pen-icon.svg'
+
+/* */
+
 import { useHistory } from "react-router";
 
 import '../Admin-panel-complete/scss/admin-panel.css'
@@ -54,9 +70,10 @@ const AdminPanel = () => {
         </div>
         <div className="admin_panel_itself">
 
+        <div className="container-for-components">
         <div className="stat_panel_itself">
         <div className="stat_panel_desc_p">
-        <h1 className="font-bold text-black text-2xl m-1">Statystyki Zdawalności</h1>
+        <h1 className="font-bold text-black text-2xl m-2">Statystyki Zdawalności</h1>
         <p className="mx-4 my-1 text-sm">Bazowane na procentach wykonanych testów i nauki</p>
         </div>
         <div className="container_for_items">
@@ -81,7 +98,7 @@ const AdminPanel = () => {
         
         <div className="licenses_panel_itself">
         <div className="licenses_panel_desc_p">
-        <img src={LicenseLogo} style={{ alignSelf: 'center', height: '50px', width: '50px', margin: '30px' }}></img>
+        <img className="img-fix" src={LicenseLogo} style={{ alignSelf: 'center', margin: '30px' }}></img>
         <h1 className="font-bold text-black text-2xl m-2">Aktywne Licencje</h1>
         <p className="mx-4 my-1 text-sm">Ilość aktywnych licencji</p>
         <div className="container_for_text_and_icon" style={{ display: 'flex', justifyContent: 'center' }}>
@@ -91,6 +108,79 @@ const AdminPanel = () => {
         </div>
         
 
+
+        </div>
+
+        <div className="google_play_panel_itself">
+        <div className="google_play_panel_desc_p">
+        <h1 className="font-bold text-black text-2xl m-2">Satysfakcja z aplikacji</h1>
+        <p className="mx-4 my-1 text-sm">Bazowane na pozytywnych ocenach z GooglePlay</p>
+        <div className="container_for_meter" style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center' }}>
+        
+        <meter class="google_play_meter" style={{ marginTop: '45px', color: 'blue' }} max="1.0" min=".0" value=".9" high=".75" low=".25" optimum=".5"></meter> 
+
+        <HeartIcon/>
+
+        <h1 className="font-bold text-black text-xl m-2">90%</h1>
+
+        </div>
+        </div>
+        
+
+
+        </div>
+
+        <div className="shortcode_panel_itself">
+        <h1 className="font-bold text-black text-2xl m-2" style={{ margin: '15px' }}>Panel skrótów</h1>
+        <div className="shortcode_panel_desc_content">
+        <ul className="shortcode-list">
+            <li> <img src={NewsIcon} ></img>Dodaj wpis na blogu</li><hr></hr>
+            <li> <img src={NotificationList} ></img> Wyślij powiadomienie do użytkowników</li><hr></hr>
+            <li> <img src={LightBuble} ></img> Baza opracowań</li><hr></hr>
+            <li> <img src={ShieldLIst} ></img> Licencje</li><hr></hr>
+            <li> <img src={FileList} ></img> Baza pytań</li>
+        </ul>
+        </div>
+        </div>
+
+        </div>
+
+        <div className="container-for-components-bottom">
+
+        <div className="calendar_panel_itself">
+        <div className="calendar_panel_container">
+        <h1 className="font-bold text-black text-2xl m-2" style={{ margin: '15px' }}>Kalendarz</h1>
+         
+         <div className="calendar_container">
+
+         </div>
+
+         </div>
+
+         <div className="notes_container">
+
+        <h1 className="font-bold text-black text-2xl m-2" style={{ margin: '15px' }}>Czwartek 12 czerwca</h1>
+        <ul className="notification-list">
+            <li>
+                <h1 className="font-bold post-title">Nowy post</h1>
+                <h2 className="font-bold post-title">Uprawnienia Budowlane 2021</h2>
+                <button className="edit-post-btn"><img style={{alignSelf: 'center'}} src={PenIconC}></img></button>
+            </li>
+            <li>
+                <h1 className="font-bold post-title">Nowy post</h1>
+                <h2 className="font-bold post-title">Uprawnienia Budowlane 2021</h2>
+                <button className="edit-post-btn"><img style={{alignSelf: 'center'}} src={PenIconC}></img></button>
+            </li>
+            <li>
+                <h1 className="font-bold post-title">Nowy post</h1>
+                <h2 className="font-bold post-title">Uprawnienia Budowlane 2021</h2>
+                <button className="edit-post-btn"><img style={{alignSelf: 'center'}} src={PenIconC}></img></button>
+            </li>
+        </ul>
+
+        </div>
+
+        </div>
 
         </div>
 
